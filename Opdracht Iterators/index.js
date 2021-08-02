9ec9158744ecb686d7d1c8c34fe8c3c9c5c396b8
Typosphere <67193118+Typosphere@users.noreply.github.com>
2021-08-02 14:52:28 +0200

    Add files via upload

---
+++ Opdracht Iterators/index.js
@@ -0,0 +1,43 @@
+//Oude stijl: loops
+
+// While loop
+
+const myColor = ["yellow", "blue", "red", "orange"];
+
+let i = 0;
+while ( i < myColor.length){
+    console.log(myColor[i]);
+    i++
+}
+
+//For loop
+
+// const myColorArray = ["purple", "grey", "green", "petrol"];
+
+// for (let i= 0; i<myColorArray.length; i++){
+//     console.log(myColorArray[i]);
+// }
+
+//Nieuwe stijl: array methods
+
+// myColorArray.forEach((color) => console.log(color));
+
+
+// 1. 5 a 6 regels
+// 2. 1 regel
+// 3. je hoeft niet steeds bij te houden, waar je bent welke iteratie
+// " forEach" laat ook zien wat je gaat doen
+
+
+// const myColor = {
+//     colorWall: "grey",
+//     colorFruit: "yellow",
+//     colorCar: "black",
+//     colorHair: "brown",
+//     colorGras: "darkgreen",
+//   };
+  
+//   for (i in myColor) {
+//     console.log(myColor[i]);
+//   }
+  
